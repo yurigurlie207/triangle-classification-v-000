e48d3054c686b36decc8ca62c5264147dd31647e
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-11 05:01:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,8 @@ class Triangle
   def kind
 
 
-    if (@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
+    if (@side1 <= 0 || @side2 <= 0 || @side3 <= 0) ||
+      (@side1 + @side2 < @side3) || (@side2 + @side3 < @side1) || (@side3 + @side1 < @side2)
       raise TriangleError
     else
       if @side1 == @side2 && @side2 == @side3
