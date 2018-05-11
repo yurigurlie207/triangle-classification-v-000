0471bd22321c5d1a204e6bc7ddf5a885315b8662
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-11 05:07:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
 
     if (@side1 <= 0 || @side2 <= 0 || @side3 <= 0) ||
       (@side1 + @side2 < @side3) || (@side2 + @side3 < @side1) || (@side3 + @side1 < @side2) ||
-      (@side1 + @side2 == @side3) || (@side2 + @side3 = @side1) || (@side1 + @side3 = @side2)
+      (@side1 + @side2 == @side3) || (@side2 + @side3 == @side1) || (@side1 + @side3 == @side2)
       raise TriangleError
     else
       if @side1 == @side2 && @side2 == @side3
